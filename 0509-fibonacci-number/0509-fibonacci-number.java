ca89dacdfde1a6e315104a66bc611295d2233746
Ayush Sharma <67716805+AyushSharmaaa@users.noreply.github.com>
2022-11-20 00:34:20 +0530

    Time: 0 ms (100.00%), Space: 39 MB (92.13%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -7,7 +7,7 @@ class Solution {
         for (int i=2;i<arr.length;i++){
             arr[i]=arr[i-1]+arr[i-2];
             if (n==1){
-                return 1;
+                return arr[1];
             }
             if (n==i){
                 index=i;
