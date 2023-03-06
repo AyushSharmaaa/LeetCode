7e7e46bf3d768ec763bbda8f23f595be018e02e9
Ayush Sharma <67716805+AyushSharmaaa@users.noreply.github.com>
2023-03-06 12:42:18 +0530

    Time: 2 ms (24.36%), Space: 42.3 MB (21.23%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,20 +1,25 @@
 class Solution {
-     public static int findKthPositive(int[] arr, int k) {
-        int x=0;
-        ArrayList<Integer> list = new ArrayList<>();
+      public static int findKthPositive(int[] arr, int k) {
+        int x=0; //index of arr[]
+        ArrayList<Integer> list = new ArrayList<>(); //Array list stores the element which is missing in arr[]
+
+        //Loop for comparing k positive integer with arr[]
         for (int i=1; i<arr[arr.length-1]; i++){
+            //check arr[x] is equal to positive integer (i)
             if (arr[x]==i){
                 x++;
             }
+            //add missing integer value in list
             else {
                 list.add(i);
             }
         }
-        System.out.print(list);
+        //k is greater than list size
         if (list.size()<k){
             k=k-list.size();
             return arr[arr.length-1]+k;
         }
+        //return the kth missing positive integer
         else {
             return list.get(k-1);
         }
